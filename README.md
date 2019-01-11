2700e5484cea3d20e0c828826f66ae732666c522
Alexander Schulz <alex@nope.bz>
2019-01-11 04:37:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+![missde5Sup](https://static-cdn.jtvnw.net/emoticons/v1/1750968/3.0 "missde5Sup")
+
+
+## Configuration
+
+Copy `config.js.dist` to `config.js` and set values accordingly.
+
+`config.tmi_opts`:
+
+* `identity.username`: your twitch username / the bots twitch username
+* `identity.password`: the oauth token for authorization
+
+
+`config.bot_opts`:
+
+* `emote`: the emote to post (f.i. VoHiYo)
+* `autoPost`: whether to automatically periodically post the emote to all connected channels
+* `autoPostDelay`: the automatic posting interval delay in milliseconds
+* `autoPostRngDelay`: set to greater than 0 if the periodic interval delay should further be delayed by a random range of +/- `autoPostRngDelay / 2`
+
+
+## TODOs
+
+* ![missde5Sup](https://static-cdn.jtvnw.net/emoticons/v1/1750968/1.0 "missde5Sup")
+* only post automatically if the channel is live
---
+++ emote_bot.js
@@ -0,0 +1,116 @@
+const tmi = require('tmi.js');
+const config = require('./config.js');
+
+console.log(config);
+
+var lastPost = Date.now();
+
+const postEmote = function (channel, prefix) {
+    let msg = config.bot_opts.emote;
+
+    if (prefix && typeof prefix === 'string' && prefix.length > 0) {
+        msg = `${prefix.trim()} ${msg}`;
+    }
+
+    lastPost = Date.now();
+    client.say(channel, msg);
+
+    console.log('* Emote posted', channel, prefix);
+};
+
+const getNextAutoPost = function () {
+    return lastPost
+               + (config.bot_opts.autoPostDelay
+                     + Math.floor(Math.random() * config.bot_opts.autoPostRngDelay)
+                     - (config.bot_opts.autoPostRngDelay / 2));
+};
+const autoPostLoop = function (channel) {
+    if (!config.bot_opts.autoPost) {
+        return;
+    }
+
+    if (Date.now() >= getNextAutoPost()) {
+        postEmote(channel);
+    }
+
+    setTimeout(autoPostLoop.bind(this, channel), getNextAutoPost() - Date.now() - 100);
+
+    console.log('* Auto post sheduled', channel, getNextAutoPost() - Date.now() - 100);
+};
+
+
+const client = new tmi.client(config.tmi_opts);
+
+client.on('message', onMessageHandler);
+client.on('subscription', onSubHandler);
+client.on('resub', onSubHandler);
+client.on('connected', onConnectedHandler);
+
+client.connect();
+
+
+const mentionRegex = new RegExp(`(?:^|[\\b@])${config.tmi_opts.identity.username.toLowerCase()}(?:\\b|$)`);
+const containsMention = function (msg) {
+    return !!msg
+               .trim()
+               .toLowerCase()
+               .match(mentionRegex);
+};
+
+const getUserFromState = function (userstate, fallback) {
+    if (typeof userstate === 'object'
+            && 'display-name' in userstate
+            && userstate['display-name']) {
+        return '@' + userstate['display-name'];
+    }
+
+    if (fallback) {
+        return '@' + fallback.trim().trim('#');
+    }
+
+    return '';
+}
+
+function onMessageHandler(channel, userstate, msg, self) {
+    //console.log('* Message received', channel, msg, self, userstate);
+
+    if (self) {
+        return;
+    }
+
+    if (userstate["message-type"] !== "chat") {
+        return;
+    }
+
+    if (containsMention(msg)) {
+        postEmote(channel, getUserFromState(userstate, userstate['username']));
+    }
+}
+
+function onSubHandler(channel, username, months, msg, userstate, method) {
+    console.log('* Subscription', channel, username, msg, months, userstate);
+
+    if (username.toLowerCase() === config.tmi_opts.identity.username.toLowerCase()) {
+        return;
+    }
+
+    let prefix = '';
+    if (typeof months === "number") {
+        prefix = config.bot_opts.emote + ' ';
+        prefix = prefix.repeat(months - 1).trim();
+    } else if (typeof months === "object") {
+        userstate = months;
+    }
+
+    username = getUserFromState(userstate, username);
+
+    postEmote(channel, `${username} ${prefix}`);
+}
+
+function onConnectedHandler(addr, port) {
+    console.log(`* Connected to ${addr}:${port}`);
+
+    for (channel of config.tmi_opts.channels) {
+        autoPostLoop(channel);
+    }
+}
